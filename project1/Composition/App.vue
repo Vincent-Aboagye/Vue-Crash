94335985ef3389f223dde54e2f8d2794336c697b
Vynx <68636740+Vincent-Aboagye@users.noreply.github.com>
2020-11-24 10:39:51 +0000

    End of Composition API

--- project1/Composition/App.vue
+++ project1/Composition/App.vue
@@ -9,30 +9,16 @@
    </div>
 </template>
 <script>
-import {ref, reactive, computed, watch, watchEffect } from 'vue'
+import {useNumbers } from './numbers'
 export default {
     
     setup(){
-        const count = ref(0)
-        const a = ref(0)
-        const b = ref(0)
-        const history = ref([])
-   
-    //using watch effect
-    watchEffect(()=>{
-        console.log(a.value, b.value)
-    })
-
-    //using watch
-    watch([a,b], ([newA, newB], [oldA, oldB])=>{
-        if(newA !== oldA){
-            history.value.push(`a: ${oldA} => ${newA}`)
-        }
-        if(newB !== oldB){
-            history.value.push(`b: ${oldB} => ${newB}`)
-        }
-    })
-        const total = computed( ()=> count.value + a.value + b.value )
+        //destructuring
+      const  {  total,
+                count,
+                history,
+                a,
+                b} = useNumbers()
         return{
             total,
             count,
