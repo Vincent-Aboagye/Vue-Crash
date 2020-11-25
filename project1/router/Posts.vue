f715230adfceccc19d3721b10e702d5ca904d6df
Vynx <68636740+Vincent-Aboagye@users.noreply.github.com>
2020-11-25 14:55:29 +0000

    Route params with composition API

--- project1/router/Posts.vue
+++ project1/router/Posts.vue
@@ -1,9 +1,19 @@
 <template>
     <h4>Posts</h4>
+    <router-link v-for="post in testPosts" :to="`/posts/${post.id}`">
+        {{post.title}}
+    </router-link>
+    <div><router-view /></div>
 </template>
 
 <script>
+import { testPosts} from '../MicoBlog/testPosts.js'
 export default {
-    
+    setup(){
+        return{
+            testPosts
+        }
+    }
+   
 }
 </script>
