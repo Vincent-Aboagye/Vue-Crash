649c4e91372c02606b75c14556625b7fe3755b8d
Vynx <68636740+Vincent-Aboagye@users.noreply.github.com>
2020-11-25 15:05:16 +0000

    Route params with options API

--- project1/router/Post.vue
+++ project1/router/Post.vue
@@ -12,16 +12,21 @@ import {computed} from 'vue'
 import { useRoute, useRouter } from "vue-router";
 import { testPosts} from '../MicoBlog/testPosts.js'
 export default {
-    setup(){
-        const route = useRoute()
-        const post = computed(()=>{
-            return testPosts.find( x => x.id === parseInt(route.params.id) )
-        })
-
-        return {
-            post
+    computed:{
+        post(){ return testPosts.find( x => x.id === parseInt(this.$route.params.id) )
         }
     }
+
+    // setup(){
+    //     const route = useRoute()
+    //     const post = computed(()=>{
+    //         return testPosts.find( x => x.id === parseInt(route.params.id) )
+    //     })
+
+    //     return {
+    //         post
+    //     }
+    // }
  
 }
 </script>
